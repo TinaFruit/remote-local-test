b71c0acc85d25efc8459364dae016739fd4d605c
Ting  Yi <tingyi@Tings-MacBook-Pro.local>
2024-05-09 22:39:46 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Classname:${NAME}
+ * Package:
+ * Description:
+ *
+ * @Create: ${DATE} - ${TIME}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!" +
+                "aaaaaa");
+    }
+}
